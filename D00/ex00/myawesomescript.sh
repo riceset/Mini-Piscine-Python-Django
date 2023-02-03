838459050ecc9408f4b72a700c98f0205a258704
komeno <tkomeno@student.42tokyo.jp>
2023-02-03 15:25:43 -0300

    ex03

--- D00/ex00/myawesomescript.sh
+++ D00/ex00/myawesomescript.sh
@@ -6,9 +6,11 @@
 #    By: tkomeno <tkomeno@student.42tokyo.jp>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/01/30 14:48:22 by tkomeno           #+#    #+#              #
-#    Updated: 2023/01/30 14:54:00 by tkomeno          ###   ########.fr        #
+#    Updated: 2023/02/01 17:37:41 by tkomeno          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+
+#!/usr/bin/sh
 
 if [ $# -ne 1 ]; then
 	echo "Error: wrong number of arguments."
