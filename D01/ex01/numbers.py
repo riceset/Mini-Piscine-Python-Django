83e0d8f6fa3ec0b5b78ca1966a3f191a78ee4d20
komeno <tkomeno@student.42tokyo.jp>
2023-02-06 13:27:18 -0300

    ex02

--- D01/ex01/numbers.py
+++ D01/ex01/numbers.py
@@ -6,12 +6,16 @@
 #    By: tkomeno <tkomeno@student.42tokyo.jp>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/02/03 16:39:09 by tkomeno           #+#    #+#              #
-#    Updated: 2023/02/03 16:46:25 by tkomeno          ###   ########.fr        #
+#    Updated: 2023/02/06 13:27:08 by tkomeno          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-with open("numbers.txt", "r") as object:
-    data = object.read()
-    lines = data.split(",")
-    for line in lines:
-        print(line.strip())
+def main():
+    with open("numbers.txt", "r") as object:
+        data = object.read()
+        lines = data.split(",")
+        for line in lines:
+            print(line.strip())
+
+if __name__ == '__main__':
+    main()
---
+++ D01/ex02/var_to_dict.py
@@ -0,0 +1,48 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    var_to_dict.py                                     :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: tkomeno <tkomeno@student.42tokyo.jp>       +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/02/06 13:26:08 by tkomeno           #+#    #+#              #
+#    Updated: 2023/02/06 13:26:18 by tkomeno          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def main():
+
+    d = [
+    ('Hendrix' , '1942'),
+    ('Allman' , '1946'),
+    ('King' , '1925'),
+    ('Clapton' , '1945'),
+    ('Johnson' , '1911'),
+    ('Berry' , '1926'),
+    ('Vaughan' , '1954'),
+    ('Cooder' , '1947'),
+    ('Page' , '1944'),
+    ('Richards' , '1943'),
+    ('Hammett' , '1962'),
+    ('Cobain' , '1967'),
+    ('Garcia' , '1942'),
+    ('Beck' , '1944'),
+    ('Santana' , '1947'),
+    ('Ramone' , '1948'),
+    ('White' , '1975'),
+    ('Frusciante', '1970'),
+    ('Thompson' , '1949'),
+    ('Burton' , '1939')
+    ]
+
+    list = []
+
+    for item in d:
+        list.append({item[1]:item[0]})
+
+    for dict in list:
+        for key in dict:
+            print(f"{key} : {dict[key]}")
+
+if __name__ == '__main__':
+    main()
